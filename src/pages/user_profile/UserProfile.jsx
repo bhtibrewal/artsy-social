cfbaa96d7a58fe007773bdb41b07248bc34e71c2
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2022-05-05 20:53:13 +0530

    remove console.log

--- src/pages/user_profile/UserProfile.jsx
+++ src/pages/user_profile/UserProfile.jsx
@@ -1,5 +1,5 @@
 import "./user_profile.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { FaRegUser } from "../../assets/icons";
 import { useAuth } from "../../contexts";
@@ -7,40 +7,44 @@ import axios from "axios";
 
 export const UserProfile = () => {
   const { username } = useParams();
-  // console.log(userId);
-  const getUser = async () => {
+  const [userToShow, setUserToShow] = useState({});
+  const { firstName, lastName, followers=[], following=[] } = userToShow;
+  const getUser = async ({ username, setUserToShow }) => {
     try {
       const res = await axios.get(`/api/users/${username}`);
-      console.log(res.data);
-      // setUserData(res);
+      setUserToShow(res.data.user);
     } catch (e) {
-      console.log(e);
+      console.error(e);
     }
   };
   useEffect(() => {
-    getUser();
+    getUser({ username, setUserToShow });
   }, [username]);
 
-  const {
-    userData: { followers, following },
-  } = useAuth();
+  const { userData } = useAuth();
 
   return (
     <main>
+      <section>
+        <p>
+          {firstName}{" "} 
+          {lastName}
+        </p>
+      </section>
       <section className="user-stats">
         <div className="stats-cards followers-card">
           <span className="stats-card-icon">
             <FaRegUser />
           </span>
           <span>Followers</span>
-          <p className="body-l "> {followers.length}+ </p>
+          <p className="body-l "> {followers?.length}+ </p>
         </div>
         <div className="stats-cards following-card">
           <span className="stats-card-icon">
             <FaRegUser />
           </span>
           <span>Following</span>
-          <p className="body-l"> {following.length}+ </p>
+          <p className="body-l"> {following?.length}+ </p>
         </div>
         <div className="stats-cards posts-card">
           <span className="stats-card-icon">
